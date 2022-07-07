575dab11d7a191ea54e8fc70b06c8c94d07cbf1e
bryant klewer <bklewer@gmail.com>
2022-07-06 20:59:32 -0400

    add queries and retirement_info.csv

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -60,12 +60,33 @@ WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1953-12-31';
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1954-12-31';
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+Select first_name, last_name
+from employees
+where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+and (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+select count(first_name)
+from employees
+where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+and (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Select first_name, last_name
+into retirement_info
+from employees
+where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+and (hire_date BETWEEN '1985-01-01' AND '1988-12-31') ;
+
+select *
+from retirement_info;
